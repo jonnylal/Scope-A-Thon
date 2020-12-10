5a16ed5685e37fc38829ce0bd74f030bfd3ceacb
Jonathan Lal <jlal@usc.edu>
2020-12-09 19:19:09 -0800

    Initializing remote repo

---
+++ README.md
@@ -0,0 +1,6 @@
+# Scope-A-Thon WebApp Defuse
+
+Dependecies:
+
+node version: v14.15.1
+yarn version: 1.22.10
---
+++ defuse/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import bomb from "./bomb.gif";
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<div className="container">
+					<h1>Defuse</h1>
+					<img src={bomb} alt="bomb" />
+				</div>
+				<button type="button" class="btn btn-danger btn-lg">
+					Get started now or else...
+				</button>
+			</header>
+		</div>
+	);
+}
+
+export default App;
